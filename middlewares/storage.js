e930fec5f04b038d104a9ef05f4fd986d9a51050
fakhryhizballah <falehry88@gmail.com>
2022-06-01 18:27:13 +0700

    bug

--- middlewares/storage.js
+++ middlewares/storage.js
@@ -8,7 +8,7 @@ const storage = multer.diskStorage({
 
     filename: function (req, file, callback) {
         const namaFile = Date.now() + '-' + file.originalname.split(' ').join('') + path.extname(file.originalname);
-        // console.log(file.originalname.split(' ').join(''));
+        console.log(file);
         callback(null, namaFile);
     }
 });
